456f35362639c0a290b2f53043b7458f5495ccb4
Amaimersion <amatsukine@gmail.com>
2019-03-20 15:29:41 +0300

    Change version to 1.1.1.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /**
  * @module RemoveFilesWebpackPlugin
- * @version 1.1.0
+ * @version 1.1.1
  * @file A plugin for webpack which removes files and folders before and after compilation.
  * @author Sergey Kuznetsov <https://github.com/Amaimersion>
  * @license MIT <https://github.com/Amaimersion/remove-files-webpack-plugin/blob/master/LICENSE>

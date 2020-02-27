495f530e5f12c892fdf1be86871701446456651f
Amaimersion <amatsukine@gmail.com>
2020-02-27 16:43:09 +0300

    Change version to 1.4.0

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for remove-files-webpack-plugin 1.3
+// Type definitions for remove-files-webpack-plugin 1.4
 // Project: https://github.com/Amaimersion/remove-files-webpack-plugin/blob/master/README.md
 // Definitions by: Sergey Kuznetsov <https://github.com/Amaimersion>
 // Definitions: https://github.com/Amaimersion/remove-files-webpack-plugin
